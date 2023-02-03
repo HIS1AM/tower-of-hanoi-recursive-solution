04f703969de5162010a316c1f10ecf6f2719be07
Mouhamad Hisham Bakri Radwan Mouhaffel <pshisham8@gmail.com>
2023-02-03 09:30:57 +0100

    Add project files.

---
+++ tower of hanoi recursive solution/Program.cs
@@ -0,0 +1,11 @@
+﻿MoveDisks(4, 1, 3, 2);
+
+static void MoveDisks(int n, int s, int f, int t)
+{
+    if(n > 0)
+    {
+        MoveDisks(n - 1, s, t, f);
+        Console.WriteLine($"move disk {n} from tower {s} to tower {f}");
+        MoveDisks(n - 1, t, f, s);
+    }
+}
